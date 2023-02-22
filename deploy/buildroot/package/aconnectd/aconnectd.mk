0262950c6bf768a4dd4a9a890d2c6fd584773144
Darryl Sokoloski <darryl@sokoloski.ca>
2023-02-22 18:23:28 +0000

    Added deployment files.

---
+++ deploy/buildroot/package/aconnectd/aconnectd.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# aconnectd
+#
+################################################################################
+
+ACONNECTD_VERSION = aconnectd
+ACONNECTD_SITE = git@git.sokoloski.ca:buildroot-rtpmidi.git
+ACONNECTD_SITE_METHOD=git
+ACONNECTD_GIT_SUBMODULES = NO
+ACONNECTD_LICENSE = GPL-3
+ACONNECTD_LICENSE_FILES = LICENCE.md
+ACONNECTD_CPE_ID_VENDOR = dsokoloski
+ACONNECTD_CPE_ID_PRODUCT = aconnectd
+ACONNECTD_DEPENDENCIES = host-pkgconf host-cmake
+ACONNECTD_INSTALL_STAGING = YES
+
+define ACONNECTD_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/systemd/aconnectd.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/aconnectd.service
+endef
+
+$(eval $(cmake-package))
