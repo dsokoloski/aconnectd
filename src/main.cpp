880439d1ce6453edcf5b6bdbeb8c89d3976e141b
Darryl Sokoloski <darryl@sokoloski.ca>
2023-02-22 23:16:09 +0000

    On-going implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ using namespace std;
 #include "aconnectd.h"
 
 static int acd_my_id = -1;
+
 static acdSubAddrMap acd_sub_addr_map;
 static acdSubMap acd_sub_map;
 
@@ -132,8 +133,9 @@ size_t acdPort::AddSubscriptions(snd_seq_t *seq,
 			)
 		);
 
-		fprintf(stdout, "Added subscriptions: %d:%d -> %d:%d: %d\n",
+		fprintf(stdout, "Added subscriptions: %d:%d %s %d:%d: %d\n",
 			client.id, id,
+			(type == SND_SEQ_QUERY_SUBS_READ) ? "->" : "<-",
 			addr->client, addr->port, type
 		);
 
@@ -145,108 +147,6 @@ size_t acdPort::AddSubscriptions(snd_seq_t *seq,
 	}
 
 	return count;
-}
-
-static void acd_get_port(snd_seq_t *seq, snd_seq_client_info_t *cinfo,
-	snd_seq_port_info_t *pinfo, int count)
-{
-	if (! count) {
-		int card = -1, pid = -1;
-
-		printf("client %d: '%s' [type=%s",
-		       snd_seq_client_info_get_client(cinfo),
-		       snd_seq_client_info_get_name(cinfo),
-		       (snd_seq_client_info_get_type(cinfo) == SND_SEQ_USER_CLIENT ?
-			"user" : "kernel")
-		);
-
-		card = snd_seq_client_info_get_card(cinfo);
-		if (card != -1)
-			printf(",card=%d", card);
-
-		pid = snd_seq_client_info_get_pid(cinfo);
-		if (pid != -1)
-			printf(",pid=%d", pid);
-		printf("]\n");
-	}
-
-	printf("  %3d '%-16s'\n",
-	       snd_seq_port_info_get_port(pinfo),
-	       snd_seq_port_info_get_name(pinfo));
-}
-
-static void acd_get_subscribers(
-	snd_seq_t *seq, snd_seq_query_subscribe_t *subs, snd_seq_query_subs_type_t type)
-{
-	int count = 0;
-	snd_seq_query_subscribe_set_type(subs, type);
-	snd_seq_query_subscribe_set_index(subs, 0);
-
-	while (snd_seq_query_port_subscribers(seq, subs) >= 0) {
-		const snd_seq_addr_t *addr;
-
-		if (count++ == 0)
-			printf("\t%d: ", type);
-		else
-			printf(", ");
-
-		addr = snd_seq_query_subscribe_get_addr(subs);
-		printf("%d:%d", addr->client, addr->port);
-
-		if (snd_seq_query_subscribe_get_exclusive(subs))
-			printf("[ex]");
-
-		if (snd_seq_query_subscribe_get_time_update(subs)) {
-			printf("[%s:%d]",
-				(snd_seq_query_subscribe_get_time_real(subs) ? "real" : "tick"),
-				snd_seq_query_subscribe_get_queue(subs)
-			);
-		}
-
-		snd_seq_query_subscribe_set_index(subs,
-			snd_seq_query_subscribe_get_index(subs) + 1
-		);
-	}
-
-	if (count > 0) printf("\n");
-}
-
-static void acd_get_subscribers(snd_seq_t *seq)
-{
-	int count;
-
-	snd_seq_client_info_t *cinfo;
-	snd_seq_client_info_alloca(&cinfo);
-	snd_seq_client_info_set_client(cinfo, -1);
-
-	snd_seq_port_info_t *pinfo;
-	snd_seq_port_info_alloca(&pinfo);
-
-	while (snd_seq_query_next_client(seq, cinfo) >= 0) {
-		snd_seq_port_info_set_client(
-			pinfo,
-			snd_seq_client_info_get_client(cinfo)
-		);
-
-		count = 0;
-		snd_seq_port_info_set_port(pinfo, -1);
-
-		while (snd_seq_query_next_port(seq, pinfo) >= 0) {
-			acd_get_port(seq, cinfo, pinfo, count);
-
-			snd_seq_query_subscribe_t *subs;
-			snd_seq_query_subscribe_alloca(&subs);
-			snd_seq_query_subscribe_set_root(
-				subs,
-				snd_seq_port_info_get_addr(pinfo)
-			);
-
-			acd_get_subscribers(seq, subs, SND_SEQ_QUERY_SUBS_READ);
-			acd_get_subscribers(seq, subs, SND_SEQ_QUERY_SUBS_WRITE);
-
-			count++;
-		}
-	}
 }
 
 static void acd_refresh(snd_seq_t *seq)
@@ -334,7 +234,8 @@ static void acd_resolve_subscriptions(void)
 			)
 		);
 
-		if (it.second == true) {
+		if (it.second == true &&
+			it_sub.second.second == SND_SEQ_QUERY_SUBS_READ) {
 			fprintf(stdout, "Resolved subscription: %s -> %s\n",
 				it.first->first.first.c_str(),
 				it.first->first.second.c_str()
@@ -355,8 +256,6 @@ int main(int argc, char *argv[])
 	snd_lib_error_set_handler(acd_error);
 
 	acd_my_id = snd_seq_client_id(seq);
-
-	acd_get_subscribers(seq);
 
 	acd_refresh(seq);
 	acd_resolve_subscriptions();
